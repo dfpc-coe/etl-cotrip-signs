9712b643cf564e693ebf10c09177ed55c3b43834
ingalls <nick@ingalls.ca>
2023-02-28 16:23:14 -0800

    Update to TAK-PS/ETL

--- task.js
+++ task.js
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import ETL from '@tak-ps/etl';
 
 try {
     const dotfile = new URL('.env', import.meta.url);
@@ -11,23 +12,7 @@ try {
     console.log('ok - no .env file loaded');
 }
 
-export default class Task {
-    constructor() {
-        this.token = process.env.COTRIP_TOKEN;
-        this.api = 'https://data.cotrip.org/';
-
-        this.etl = {
-            api: process.env.ETL_API,
-            layer: process.env.ETL_LAYER,
-            token: process.env.ETL_TOKEN
-        };
-
-        if (!this.token) throw new Error('No COTrip API Token Provided');
-        if (!this.etl.api) throw new Error('No ETL API URL Provided');
-        if (!this.etl.layer) throw new Error('No ETL Layer Provided');
-        // if (!this.etl.token) throw new Error('No ETL Token Provided');
-    }
-
+export default class Task extends ETL {
     static schema() {
         return {
             type: 'object',
@@ -47,13 +32,19 @@ export default class Task {
     }
 
     async control() {
+        const layer = await this.layer();
+
+        const api = 'https://data.cotrip.org/';
+        if (!layer.data.environment.COTRIP_TOKEN) throw new Error('No COTrip API Token Provided');
+        const token = layer.data.environment.COTRIP_TOKEN;
+
         const incidents = [];
         let batch = -1;
         let res;
         do {
             console.log(`ok - fetching ${++batch} of incidents`);
-            const url = new URL('/api/v1/incidents', this.api);
-            url.searchParams.append('apiKey', this.token);
+            const url = new URL('/api/v1/incidents', api);
+            url.searchParams.append('apiKey', token);
             if (res) url.searchParams.append('offset', res.headers.get('next-offset'));
 
             res = await fetch(url);
@@ -91,23 +82,7 @@ export default class Task {
             features: features
         };
 
-        if (process.env.DEBUG) for (const feat of fc.features) console.error(JSON.stringify(feat));
-
-        const post = await fetch(new URL(`/api/layer/${this.etl.layer}/cot`, this.etl.api), {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${this.etl.token}`,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(fc)
-        });
-
-        if (!post.ok) {
-            console.error(await post.text());
-            throw new Error('Failed to post layer to ETL');
-        } else {
-            console.log(await post.json());
-        }
+        await this.submit(fc);
     }
 }
 
